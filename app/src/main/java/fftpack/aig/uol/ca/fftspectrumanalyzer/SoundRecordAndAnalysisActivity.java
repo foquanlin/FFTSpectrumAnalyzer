275699aa0b734d0a224da737f9397a410aa6f750
lucien-george <lucien.george@mail.mcgill.ca>
2018-03-28 19:25:59 -0500

    Sound frequencies displayed on both graph correctly

--- app/src/main/java/fftpack/aig/uol/ca/fftspectrumanalyzer/SoundRecordAndAnalysisActivity.java
+++ app/src/main/java/fftpack/aig/uol/ca/fftspectrumanalyzer/SoundRecordAndAnalysisActivity.java
@@ -79,7 +79,7 @@ public class SoundRecordAndAnalysisActivity extends AppCompatActivity {
 //    private DisplayMetrics displayM;
     private float xmax , xmin , ymax , ymin;
 
-    private LineGraphSeries<DataPoint> frq_series , min_series , max_series , sound_series; // data for the graph
+    private LineGraphSeries<DataPoint> min_series , max_series , sound_series; // data for the graph
     private BarGraphSeries<DataPoint> bicep_series , triceps_series , forearm_series;
     private double x_frq, y_frq , x_bicep , y_bicep , x_min , y_min , x_max , y_max; // x_frq and y_frq coordinates
     private int BICEP_FRQ = 1000 , TRICEPS_FRQ = 2000 , FOREARM_FRQ = 4000 , MAX_MAGNITUDE = 400 , MIN_MAGNITUDE = 50;
@@ -92,25 +92,15 @@ public class SoundRecordAndAnalysisActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sound_record_and_analysis); // call the UI
         setUpVariables();
         requestRecordAudioPermission();
-        setUpGraph();
     }
 
     private void setUpVariables() { // initializing all views with their respective ids
-        graphView = (GraphView) findViewById(R.id.frequency_spectrum);
         bicep_txtview = (TextView) findViewById(R.id.bicep_tv);
         bicep_frq_et = (EditText) findViewById(R.id.bicep_frq_et);
         triceps_txtview = (TextView) findViewById(R.id.triceps_tv);
         triceps_frq_et = (EditText) findViewById(R.id.triceps_frq_et);
         forearm_txtview = (TextView) findViewById(R.id.forearm_tv);
         forearm_frq_et = (EditText) findViewById(R.id.forearm_frq_et);
-//        bitmap = (ImageView) findViewById(R.id.bitmap_iv);
-        frq_series = new LineGraphSeries<DataPoint>();
-        min_series = new LineGraphSeries<DataPoint>();
-        max_series = new LineGraphSeries<DataPoint>();
-        bicep_series = new BarGraphSeries<DataPoint>();
-        triceps_series = new BarGraphSeries<DataPoint>();
-        forearm_series = new BarGraphSeries<DataPoint>();
-        sound_series = new LineGraphSeries<DataPoint>();
         update_frq_button = (Button) findViewById(R.id.update_frq_btn);
         replay_recording_button = (Button) findViewById(R.id.replay_recording_btn);
         start_recording_button = (Button) findViewById(R.id.start_recording_btn);
@@ -122,90 +112,6 @@ public class SoundRecordAndAnalysisActivity extends AppCompatActivity {
         display = getWindowManager().getDefaultDisplay();
         width = display.getWidth();
         height = display.getHeight() / 4;
-    }
-
-    private void setUpGraph() {
-        display = getWindowManager().getDefaultDisplay(); // get device screen info
-        int height = display.getHeight(); // get height of screen
-        int graphViewHeight = height / 4; // graph is 1/4 of the screen's height
-        graphView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT , graphViewHeight)); // set graph's width and height
-        graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() { // set labels for x_frq and y_frq axis
-            @Override
-            public String formatLabel(double value , boolean isValueX) {
-                if (isValueX) {
-                    // show normal x_frq values
-                    return super.formatLabel(value, isValueX) + "kHz";
-                } else {
-                    // show currency for y_frq values
-                    return super.formatLabel(value, isValueX) + "dB";
-                }
-            }
-        });
-        graphView.getViewport().setXAxisBoundsManual(true); // set range of x-axis
-        graphView.getViewport().setMinX(0.0);
-        graphView.getViewport().setMaxX(21);
-        graphView.getViewport().setYAxisBoundsManual(true); // set range of y-axis
-        graphView.getViewport().setMinY(0.0);
-        graphView.getViewport().setMaxY(500);
-        graphView.getViewport().setScalable(true); // make zooming and scrolling active x-axis
-        graphView.getViewport().setScalableY(true); // make zooming and scrolling active y-axis
-        x_frq = 0; // first x_frq value
-        for(int i = 0 ; i < 1000 ; i++) { // display the wanted function through the loop
-            x_frq = x_frq + 0.1f;
-            y_frq = 2 * Math.pow(x_frq, 2); // TODO: Implement real data
-            frq_series.appendData(new DataPoint(x_frq, y_frq) , true , 1000);
-        }
-        graphView.addSeries(frq_series); // add the data to the graph
-
-        // setting up min line
-        x_min = 0;
-        Paint min_paint = new Paint();
-        setDashPaint(min_series , min_paint , Color.GREEN);
-        for(int j = 0 ; j < 1000 ; j++) {
-            x_min = x_min + 0.1f;
-            y_min = MIN_MAGNITUDE;
-            min_series.appendData(new DataPoint(x_min , y_min) , true , 1000);
-        }
-        graphView.addSeries(min_series);
-
-        // setting up max line
-        x_max = 0;
-        Paint max_paint = new Paint();
-        setDashPaint(max_series , max_paint , Color.RED);
-        for(int j = 0 ; j < 1000 ; j++) {
-            x_max = x_max + 0.1f;
-            y_max = MAX_MAGNITUDE;
-            max_series.appendData(new DataPoint(x_max , y_max) , true , 1000);
-        }
-        graphView.addSeries(max_series);
-
-        // set target frq for bicep
-        setTargetFrequencyLines(bicep_series , Color.CYAN , BICEP_FRQ);
-
-        // set target frq for triceps
-        setTargetFrequencyLines(triceps_series , Color.YELLOW , TRICEPS_FRQ);
-
-        // set target frq for forearm
-        setTargetFrequencyLines(forearm_series , Color.MAGENTA , FOREARM_FRQ);
-    }
-
-    // set max and min magnitude dashed lines
-    public void setDashPaint(LineGraphSeries series , Paint paint , int color) {
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(3);
-        paint.setColor(color);
-        DashPathEffect dashPathEffect = new DashPathEffect(new float[]{8, 5}, 0);
-        paint.setPathEffect(dashPathEffect);
-        paint.setAntiAlias(true);
-        series.setCustomPaint(paint);
-    }
-
-    // set target frequencies
-    public void setTargetFrequencyLines(BarGraphSeries series , int color , int frq) {
-        series.appendData(new DataPoint(frq / 1000 , 1000) , true , 1);
-        series.setDataWidth(0.2f);
-        series.setColor(color);
-        graphView.addSeries(series);
     }
 
     // update frequency button
@@ -240,12 +146,6 @@ public class SoundRecordAndAnalysisActivity extends AppCompatActivity {
             forearm_series.resetData(dataPoint);
             graphView.addSeries(forearm_series);
         } else {}
-    }
-
-    public void generateData(int frq , BarGraphSeries<DataPoint> series) {
-        series.resetData(new DataPoint[]{
-                new DataPoint(frq / 1000, 1000)
-        });
     }
 
     // start recording button
@@ -319,7 +219,7 @@ public class SoundRecordAndAnalysisActivity extends AppCompatActivity {
 
     private class RecordAudio extends AsyncTask<Void , double[] , Boolean> {
         private ImageView spectrum;
-        private Paint paintScale , paintAxis, paintSpectrumDisplay;
+        private Paint paintScale , paintAxis, paintSpectrumDisplay , seriesPaint;
         private Bitmap bitmapScale;
         private Canvas canvasScale;
         private int width, height, width_bitmap , height_bitmap;
@@ -327,7 +227,6 @@ public class SoundRecordAndAnalysisActivity extends AppCompatActivity {
         private AudioRecord audioRecord;
         private DisplayMetrics displayM;
         private float xmax , xmin , ymax , ymin;
-
         private final double[] CANCELLED = {100};
         int blockSize = /*2048;// = */256;
         boolean started = false;
@@ -339,6 +238,9 @@ public class SoundRecordAndAnalysisActivity extends AppCompatActivity {
         int sampleRate = 44100;
         int channelConfiguration = AudioFormat.CHANNEL_IN_MONO;
         int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
+        DataPoint[] dataPoints;
+        private Runnable mTimer;
+
 
         public final static String IO_FILENAME= "KISDataREC";
         public FileOutputStream fOut;
@@ -367,8 +269,108 @@ public class SoundRecordAndAnalysisActivity extends AppCompatActivity {
 
         }
 
-        public void setUpSpectrum() {
+        private DataPoint[] generateData(double[][] progress) {
+            int count = 128;
+            DataPoint[] values = new DataPoint[count];
+            for(int i = 0 ; i < count ; i++) {
+                double x = i;
+                double y = progress[0][i] * 10;
+                DataPoint v = new DataPoint(x , y);
+                values[i] = v;
+            }
+            return values;
+        }
 
+        private void setUpGraph() {
+            graphView = findViewById(R.id.frequency_spectrum);
+            min_series = new LineGraphSeries<DataPoint>();
+            max_series = new LineGraphSeries<DataPoint>();
+            bicep_series = new BarGraphSeries<DataPoint>();
+            triceps_series = new BarGraphSeries<DataPoint>();
+            forearm_series = new BarGraphSeries<DataPoint>();
+            sound_series = new LineGraphSeries<DataPoint>();
+            display = getWindowManager().getDefaultDisplay(); // get device screen info
+            int height = display.getHeight(); // get height of screen
+            int graphViewHeight = height / 4; // graph is 1/4 of the screen's height
+            graphView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT , graphViewHeight)); // set graph's width and height
+            graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() { // set labels for x_frq and y_frq axis
+                @Override
+                public String formatLabel(double value , boolean isValueX) {
+                    if (isValueX) {
+                        // show normal x_frq values
+                        return super.formatLabel(value, isValueX) + "kHz";
+                    } else {
+                        // show currency for y_frq values
+                        return super.formatLabel(value, isValueX) + "dB";
+                    }
+                }
+            });
+            graphView.getViewport().setXAxisBoundsManual(true); // set range of x-axis
+            graphView.getViewport().setMinX(0.0);
+            graphView.getViewport().setMaxX(21);
+            graphView.getViewport().setYAxisBoundsManual(true); // set range of y-axis
+            graphView.getViewport().setMinY(0.0);
+            graphView.getViewport().setMaxY(500);
+            graphView.getViewport().setScalable(true); // make zooming and scrolling active x-axis
+            graphView.getViewport().setScalableY(true); // make zooming and scrolling active y-axis
+
+            // setting up min line
+            x_min = 0;
+            Paint min_paint = new Paint();
+            setDashPaint(min_series , min_paint , Color.GREEN);
+            for(int j = 0 ; j < 1000 ; j++) {
+                x_min = x_min + 0.1f;
+                y_min = MIN_MAGNITUDE;
+                min_series.appendData(new DataPoint(x_min , y_min) , true , 1000);
+            }
+            graphView.addSeries(min_series);
+
+            // setting up max line
+            x_max = 0;
+            Paint max_paint = new Paint();
+            setDashPaint(max_series , max_paint , Color.RED);
+            for(int j = 0 ; j < 1000 ; j++) {
+                x_max = x_max + 0.1f;
+                y_max = MAX_MAGNITUDE;
+                max_series.appendData(new DataPoint(x_max , y_max) , true , 1000);
+            }
+            graphView.addSeries(max_series);
+
+            // set target frq for bicep
+            setTargetFrequencyLines(bicep_series , Color.CYAN , BICEP_FRQ);
+
+            // set target frq for triceps
+            setTargetFrequencyLines(triceps_series , Color.YELLOW , TRICEPS_FRQ);
+
+            // set target frq for forearm
+            setTargetFrequencyLines(forearm_series , Color.MAGENTA , FOREARM_FRQ);
+
+            seriesPaint = new Paint();
+            seriesPaint.setColor(Color.BLUE);
+            seriesPaint.setStyle(FILL_AND_STROKE);
+            seriesPaint.setStrokeWidth(6);
+        }
+
+        // set max and min magnitude dashed lines
+        public void setDashPaint(LineGraphSeries series , Paint paint , int color) {
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(3);
+            paint.setColor(color);
+            DashPathEffect dashPathEffect = new DashPathEffect(new float[]{8, 5}, 0);
+            paint.setPathEffect(dashPathEffect);
+            paint.setAntiAlias(true);
+            series.setCustomPaint(paint);
+        }
+
+        // set target frequencies
+        public void setTargetFrequencyLines(BarGraphSeries series , int color , int frq) {
+            series.appendData(new DataPoint(frq / 1000 , 1000) , true , 1);
+            series.setDataWidth(0.2f);
+            series.setColor(color);
+            graphView.addSeries(series);
+        }
+
+        public void setUpSpectrum() {
             spectrum.setLayoutParams(new LinearLayout.LayoutParams(width , height));
             bitmapScale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
             width_bitmap = bitmapScale.getWidth();
@@ -489,6 +491,7 @@ public class SoundRecordAndAnalysisActivity extends AppCompatActivity {
         protected void onPreExecute() {
             super.onPreExecute();
             transformer = new RealDoubleFFT(blockSize);
+            dataPoints = new DataPoint[]{new DataPoint(1 , 1)};
         }
 
         @Override
@@ -614,6 +617,16 @@ public class SoundRecordAndAnalysisActivity extends AppCompatActivity {
         protected void onProgressUpdate(double[]...progress) {
             Log.e("RecordingProgress", "Displaying in progress");
             canvasScale.drawColor(Color.WHITE);
+//            mTimer = new Runnable() {
+//                @Override
+//                public void run() {
+//                    graphView.removeAllSeries();
+//                    sound_series.resetData(generateData(progress));
+//                }
+//            };
+            graphView.removeSeries(sound_series);
+            sound_series.resetData(generateData(progress));
+//            frq_series.resetData(dataPoints);
             setUpSpectrum();
             double mMaxFFTSample = 150.0;
             Log.d("Test:", Integer.toString(progress[0].length));
@@ -631,6 +644,13 @@ public class SoundRecordAndAnalysisActivity extends AppCompatActivity {
                     upper = height - 40;
                     canvasScale.drawLine(fs, lower, fs, upper , paintSpectrumDisplay);
             }
+
+            sound_series = new LineGraphSeries<>(generateData(progress));
+//            sound_series.setColor(Color.BLUE);
+            sound_series.setCustomPaint(seriesPaint);
+            sound_series.setBackgroundColor(Color.rgb(0 , 157 , 255));
+            sound_series.setDrawBackground(true);
+            graphView.addSeries(sound_series);
             canvasScale.drawLine(line_position_Bicep , height_bitmap - 40 , line_position_Bicep , 0 , paintBicep);
             canvasScale.drawLine(line_position_Triceps , height_bitmap - 40 , line_position_Triceps , 0 , paintTricep);
             canvasScale.drawLine(line_position_Forearm , height_bitmap - 40 , line_position_Forearm , 0 , paintForearm);
@@ -654,6 +674,7 @@ public class SoundRecordAndAnalysisActivity extends AppCompatActivity {
     public void onStart() {
         super.onStart();
         recordTask = new RecordAudio(spectrum , paintScale , paintAxis , paintSpectrumDisplay , bitmapScale , canvasScale , width_bitmap , height_bitmap , xmax , xmin , ymax , ymin , width , height);
+        recordTask.setUpGraph();
         recordTask.setUpSpectrum();
         recordTask.execute();
     }
